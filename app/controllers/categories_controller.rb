6d7db9ed625c11b5e31b47021cbe1c931c079d4f
Spike Vinz <spikevinz@gmail.com>
2022-06-07 10:14:40 +0800

    install hotwire

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,29 @@
 class CategoriesController < ApplicationController
   def index
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category= Category.new(category_params)
+
+    respond_to do |format|
+      if @post.save
+        # format.turbo_stream { render turbo_stream: turbo_stream.prepend('posts', partial: 'posts/post', locals: {post: @post}) }
+        format.html { redirect_to root_url, flash[:sucess] = "Category Successfully Created!" }
+        # format.json { render :show, status: :created, location: @post }
+      else
+        # format.html { render :new, status: :unprocessable_entity }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,12 +1,3 @@
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
+import "@hotwired/turbo-rails";
+import "../controllers";
 import "./tailwind.css";
