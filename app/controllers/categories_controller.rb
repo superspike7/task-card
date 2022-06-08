35e40f242aaf2cd24f3de283d76fa2304f4de7ce
Spike Vinz <spikevinz@gmail.com>
2022-06-08 20:35:47 +0800

    add create category functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,9 +10,9 @@ class CategoriesController < ApplicationController
     @category= Category.new(category_params)
 
     respond_to do |format|
-      if @post.save
-        # format.turbo_stream { render turbo_stream: turbo_stream.prepend('posts', partial: 'posts/post', locals: {post: @post}) }
-        format.html { redirect_to root_url, flash[:sucess] = "Category Successfully Created!" }
+      if @category.save
+        format.turbo_stream { flash.now[:success] = "Category Successfully Created!" }
+        format.html { redirect_to root_url, flash[:success] = "Category Successfully Created!" }
         # format.json { render :show, status: :created, location: @post }
       else
         # format.html { render :new, status: :unprocessable_entity }
