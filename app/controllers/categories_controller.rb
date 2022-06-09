9056afb4a8aed1ef8ee5a5ada5f6b4f1c8de0edb
Spike Vinz <spikevinz@gmail.com>
2022-06-09 11:42:51 +0800

    add edit category functionality

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,10 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
   def create
     @category = Category.new(category_params)
 
@@ -16,7 +20,22 @@ class CategoriesController < ApplicationController
         format.html { redirect_to root_url, flash[:success] = 'Category Successfully Created!' }
         # format.json { render :show, status: :created, location: @post }
       else
-        # format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
+        # format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update 
+    @category = Category.find(params[:id])
+    
+    respond_to do |format|
+      if @category.update(category_params)
+        format.turbo_stream { flash.now[:success] = 'Category Successfully Updated!' }
+        format.html { redirect_to root_url, flash[:success] = 'Category Successfully Updated!' }
+        # format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
         # format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
