fbaba9679731a5dc14c577f785c612995ac95cf4
Spike Vinz <spikevinz@gmail.com>
2022-06-09 11:27:18 +0800

    add system test case for editing category

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -13,4 +13,19 @@ class CategoriesTest < ApplicationSystemTestCase
     assert_selector "p", text: "Category Successfully Created!"
     assert_selector "h2", text: "To Do"
   end
+
+  test "edit existing category" do
+    visit root_url
+    assert_selector "h3", text: "Categories"
+
+    click_on "edit", match: :first
+    assert_selector "label", text: "Name"
+
+    fill_in "Name", with: "Edited Name"
+
+    click_on "Update Category"
+
+    assert_selector "p", text: "Category Successfully Edited!"
+    assert_selector "h2", text: "Edited Name"
+  end
 end
