4e86a21538f1d9e48fc669d2ff70583ce16fee64
Spike Vinz <spikevinz@gmail.com>
2022-06-11 19:03:17 +0800

    adds user registration functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions', registrations: 'users/registrations'
+    sessions: 'users/sessions', registrations: 'users/registrations', passwords: 'users/passwords'
   }
 
   resources :categories do
