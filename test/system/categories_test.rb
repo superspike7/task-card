86e24fda66063a36998f50b0e2286ff96f5ba4ab
Spike Vinz <spikevinz@gmail.com>
2022-06-11 10:24:44 +0800

    generate task model
    add system test for task

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -1,41 +1,41 @@
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class CategoriesTest < ApplicationSystemTestCase
-  test "create new category" do
-    visit root_url 
-    assert_selector "h3", text: "Categories"
+  test 'create new category' do
+    visit root_url
+    assert_selector 'h3', text: 'Categories'
 
-    click_on "New Category"
+    click_on 'New Category'
 
-    fill_in "Name", with: "To Do"
+    fill_in 'Name', with: 'To Do'
 
-    click_on "Create Category"
+    click_on 'Create Category'
 
-    assert_selector "p", text: "Category Successfully Created!"
-    assert_selector "h2", text: "To Do"
+    assert_selector 'p', text: 'Category Successfully Created!'
+    assert_selector 'h2', text: 'To Do'
   end
 
-  test "edit existing category" do
+  test 'edit existing category' do
     visit root_url
-    assert_selector "h3", text: "Categories"
+    assert_selector 'h3', text: 'Categories'
 
-    click_on "edit", match: :first
-    assert_selector "label", text: "Name"
+    click_on 'edit', match: :first
+    assert_selector 'label', text: 'Name'
 
-    fill_in "Name", with: "Edited Name"
+    fill_in 'Name', with: 'Edited Name'
 
-    click_on "Update Category"
+    click_on 'Update Category'
 
-    assert_selector "p", text: "Category Successfully Updated!"
-    assert_selector "h2", text: "Edited Name"
+    assert_selector 'p', text: 'Category Successfully Updated!'
+    assert_selector 'h2', text: 'Edited Name'
   end
 
-  test "show category details" do
+  test 'show category details' do
     visit root_url
-    assert_selector "h3", text: "Categories"
+    assert_selector 'h3', text: 'Categories'
 
-    click_on "Done"
+    click_on 'Done'
 
-    assert_selector "h1", text: "Done Details"
+    assert_selector 'h1', text: 'Done Details'
   end
 end
