59b7ede725e60abe639203341de2dc085ad1adec
Spike Vinz <spikevinz@gmail.com>
2022-06-16 09:13:21 +0800

    add category creation callback on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   has_many :categories
   has_many :tasks, through: :categories
+
+  after_commit :set_categories, on: :create
+
+  def set_categories
+    self.categories.create(name: 'To Do')
+    self.categories.create(name: 'Done')
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create default categories after create" do
+    email = 'dave@example.com'
+    password = 'secret123'
+
+    user = User.new(email: email, password: password, password_confirmation: password)
+    user.save!
+
+    assert User.find(user.id)
+    assert user.categories.find_by(name: 'To Do')
+    assert user.categories.find_by(name: 'Done')
+  end
 end
